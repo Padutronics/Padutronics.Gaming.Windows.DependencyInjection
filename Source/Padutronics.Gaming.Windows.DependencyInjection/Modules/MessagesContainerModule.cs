5c0db0a68a662786c560e81693b17503664603a2
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-28 12:47:46 +0200

    Register type Padutronics.Gaming.Windows.Messages.MouseMessageHandler

--- Source/Padutronics.Gaming.Windows.DependencyInjection/Modules/MessagesContainerModule.cs
+++ Source/Padutronics.Gaming.Windows.DependencyInjection/Modules/MessagesContainerModule.cs
@@ -1,6 +1,7 @@
 using Padutronics.DependencyInjection;
 using Padutronics.Gaming.Inputs;
 using Padutronics.Gaming.Inputs.Keyboards;
+using Padutronics.Gaming.Inputs.Mouses;
 using Padutronics.Gaming.Windows.Messages;
 
 namespace Padutronics.Gaming.Windows.DependencyInjection.Modules;
@@ -11,5 +12,6 @@ internal sealed class MessagesContainerModule : IContainerModule
     {
         containerBuilder.For<IMessageHandler>().Use<DestroyMessageHandler>().SingleInstance();
         containerBuilder.For<IMessageHandler, IInputDeviceMonitor<KeyboardState>>().Use<KeyboardMessageHandler>().SingleInstance();
+        containerBuilder.For<IMessageHandler, IInputDeviceMonitor<MouseState>>().Use<MouseMessageHandler>().SingleInstance();
     }
 }
