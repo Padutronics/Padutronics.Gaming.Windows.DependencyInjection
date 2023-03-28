ff1fd26b6a30e24f3b5b41a30515fa6ab6aa2e79
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-28 12:31:42 +0200

    Add root container module

---
+++ Source/Padutronics.Gaming.Windows.DependencyInjection/Modules/GamingWindowsContainerModule.cs
@@ -0,0 +1,10 @@
+using Padutronics.DependencyInjection;
+
+namespace Padutronics.Gaming.Windows.DependencyInjection.Modules;
+
+public sealed class GamingWindowsContainerModule : IContainerModule
+{
+    public void Load(IContainerBuilder containerBuilder)
+    {
+    }
+}
