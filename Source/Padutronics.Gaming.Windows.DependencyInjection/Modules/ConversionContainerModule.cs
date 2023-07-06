bd18e5db54891ac39887cb7c79dd74f826d70cb2
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-07-06 23:44:20 +0200

    Register type Padutronics.Gaming.Windows.Conversion.Converters.D2D_POINT_2FToOffset2DoubleConverter

--- Source/Padutronics.Gaming.Windows.DependencyInjection/Modules/ConversionContainerModule.cs
+++ Source/Padutronics.Gaming.Windows.DependencyInjection/Modules/ConversionContainerModule.cs
@@ -41,6 +41,7 @@ internal sealed class ConversionContainerModule : IContainerModule
 
         RegisterConverter<D3DCOLORVALUE, Color, D3DCOLORVALUEToColorConverter>(containerBuilder);
         RegisterConverter<D2D_MATRIX_3X2_F, Matrix3x2<double>, D2D_MATRIX_3X2_FToMatrix3x2DoubleConverter>(containerBuilder);
+        RegisterConverter<D2D_POINT_2F, Offset2<double>, D2D_POINT_2FToOffset2DoubleConverter>(containerBuilder);
         RegisterConverter<VK, Key, VKToKeyConverter>(containerBuilder);
     }
 
